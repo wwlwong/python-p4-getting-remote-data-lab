fdbfec64e830e5e316b120471fe642f3c5d5f971
William Wong <william.wai.lim.wong@gmail.com>
2023-09-02 22:20:34 -0400

    passes tests

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -3,11 +3,19 @@ import json
 
 class GetRequester:
 
+    #url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        json_list = []
+        data_dicts = json.loads(self.get_response_body())
+        for data_dict in data_dicts:
+            json_list.append(data_dict)
+        
+        return json_list
